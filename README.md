8d8cf26af7954f3c6dff1e55e21909086b55869d
Andre Freitas <andrecfreitas58@gmail.com>
2022-12-23 01:09:09 +0000

    chore: update license and readme

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-# ulauncher-chrome-profiles
+# ulauncher-brave-profiles
 
-> [Ulauncher](https://ulauncher.io) extension for launching Google Chrome using a chosen user profile.
-
-## Screenshot
-
-![screenshot](screenshot.png)
+> [Ulauncher](https://ulauncher.io) extension for launching Brave Browser using a chosen user profile.
 
 ## Requirements
 
@@ -16,22 +12,14 @@
 Open ulauncher preferences window -> extensions -> add extension and paste the following url:
 
 ```
-https://github.com/floydjohn/ulauncher-chrome-profiles
+https://github.com/andresatierf/ulauncher-brave-profiles
 ```
-
-## Contributing
-
-- Fork it!
-- Create your feature branch: git checkout -b feature/my-feature
-- Commit your changes: git commit -am 'Add some feature'
-- Push to the branch: git push origin my-new-feature
-- Submit a pull request :D
 
 ## Authors
 
-- [Alessandro Racheli](https://github.com/floydjohn)
-- README.md copied from [Bruno Paz](https://github.com/brpaz)
+- Original author [Alessandro Racheli](https://github.com/floydjohn)
+- Modified by [Andre Freitas](https://github.com/andresatierf)
 
 ## License
 
-MIT &copy; [Alessandro Racheli]
+MIT &copy; [Andre Freitas]
--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAct
 def scan_brave_folder(brave_config_folder):
     profiles = {}
     # First, let's extract profiles from Local State JSON
-    with open(os.path.join(brave_config_folder, 'Local State')) as f:
+    with open(os.path.join(brave_config_folder, "Local State")) as f:
         local_state = json.load(f)
-        cache = local_state['profile']['info_cache']
+        cache = local_state["profile"]["info_cache"]
         for folder, profile_data in cache.items():
             profiles[folder] = {
-                'name': profile_data['name'],
-                'email': profile_data['user_name']
+                "name": profile_data["name"],
+                "email": profile_data["user_name"],
             }
 
     # Leave out every past profile which doesn't exist anymore
@@ -40,7 +40,7 @@ class BraveProfilesExtension(Extension):
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        brave_config_folder = os.path.expanduser(extension.preferences['brave_folder'])
+        brave_config_folder = os.path.expanduser(extension.preferences["brave_folder"])
         profiles = scan_brave_folder(brave_config_folder)
 
         # Filter by query if inserted
@@ -48,31 +48,41 @@ class KeywordQueryEventListener(EventListener):
         if query:
             query = query.strip().lower()
             for folder in list(profiles.keys()):
-                name = profiles[folder]['name'].lower()
+                name = profiles[folder]["name"].lower()
                 if query not in name:
                     profiles.pop(folder)
 
         # Create launcher entries
         entries = []
         for folder in profiles:
-            entries.append(ExtensionResultItem(
-                icon='images/icon.png',
-                name=profiles[folder]['name'],
-                description=profiles[folder]['email'],
-                on_enter=ExtensionCustomAction({
-                    'brave_cmd': extension.preferences['brave_cmd'],
-                    'opt': ['--profile-directory={0}'.format(folder)]
-                }, keep_app_open=False)
-            ))
-        entries.append(ExtensionResultItem(
-            icon='images/incognito.png',
-            name='Incognito',
-            description='Launch browser in incognito mode',
-            on_enter=ExtensionCustomAction({
-                'brave_cmd': extension.preferences['brave_cmd'],
-                'opt': ['--incognito']
-            }, keep_app_open=False)
-        ))
+            entries.append(
+                ExtensionResultItem(
+                    icon="images/icon.png",
+                    name=profiles[folder]["name"],
+                    description=profiles[folder]["email"],
+                    on_enter=ExtensionCustomAction(
+                        {
+                            "brave_cmd": extension.preferences["brave_cmd"],
+                            "opt": ["--profile-directory={0}".format(folder)],
+                        },
+                        keep_app_open=False,
+                    ),
+                )
+            )
+        entries.append(
+            ExtensionResultItem(
+                icon="images/incognito.png",
+                name="Incognito",
+                description="Launch browser in incognito mode",
+                on_enter=ExtensionCustomAction(
+                    {
+                        "brave_cmd": extension.preferences["brave_cmd"],
+                        "opt": ["--incognito"],
+                    },
+                    keep_app_open=False,
+                ),
+            )
+        )
         return RenderResultListAction(entries)
 
 
@@ -80,10 +90,10 @@ class ItemEnterEventListener(EventListener):
     def on_event(self, event, extension):
         # Open brave when user selects an entry
         data = event.get_data()
-        brave_path = data['brave_cmd']
-        opt = data['opt']
+        brave_path = data["brave_cmd"]
+        opt = data["opt"]
         subprocess.Popen([brave_path] + opt)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     BraveProfilesExtension().run()
